95bcaf17a1342b78d9d69f8219fa82da40c8fe44
Christopher Analikwu <analikwuchristopher@gmail.com>
2022-05-09 12:00:04 +0200

    building the header

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# mobodashboard
+# Project
+
+This project is a sample analytics dashboard for a startup
+
+## Tech Stack
+
+This project was built using React, Css, Material UI and Chart Js.
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">MOBOAdmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
